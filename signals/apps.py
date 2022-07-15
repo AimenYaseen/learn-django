ea619f8b3a9d18c9d6e826738d5a7b9c066a38da
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-07-15 14:00:45 +0500

    Built in Auth Signals : user_logged_in, user_logged_out, user_login_failed

--- signals/apps.py
+++ signals/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SignalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'signals'
+
+    def ready(self):
+        from . import auth_signals
+        return super().ready()
---
+++ signals/auth_signals.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+# USER LOGGED IN SUCCESSFULLY
+@receiver(user_logged_in, sender=User)
+def login_successful(sender, request, user, **kwargs):
+    print("----------------------------------------")
+    print("Login Successful!")
+    print("User : ", sender)
+    print("Request : ", request)
+    print("User : ", user)
+    print("keyword Arguments : ", kwargs)
+    print("----------------------------------------")
+
+# Connect the signal user_logged_in.connect(receiver, sender)
+# user_logged_in.connect(login_successful, sender=User)
+
+# USER LOGIN FAILED
+@receiver(user_login_failed)
+def login_fail(sender, request, credentials, **kwargs):
+    print("----------------------------------------")
+    print("Login Fail!")
+    print("User : ", sender)
+    print("Request : ", request)
+    print("Credentials : ", credentials)
+    print("keyword Arguments : ", kwargs)
+    print("----------------------------------------")
+
+# USER LOGGED OUT SUCCESSFULLY
+@receiver(user_logged_out, sender=User)
+def logout_successful(sender, request, user, **kwargs):
+    print("----------------------------------------")
+    print("Logout Successful!")
+    print("User : ", sender)
+    print("Request : ", request)
+    print("User : ", user)
+    print("keyword Arguments : ", kwargs)
+    print("----------------------------------------")
